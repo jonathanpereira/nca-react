ae08cca606b288481355876c51ebbfd2a51110b5
JonathanPereira <jonathan@jonathanpereira.com>
2020-10-05 12:04:20 +0200

    Updates

---
+++ precache-manifest.41f13a6caf67c642d11a4b60598951ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de64ab790f4c532688984da95ec2173a",
+    "url": "/nca-react/index.html"
+  },
+  {
+    "revision": "25428990c6d730e232ac",
+    "url": "/nca-react/static/css/main.99c386bc.chunk.css"
+  },
+  {
+    "revision": "e7a9d897730a633c2722",
+    "url": "/nca-react/static/js/2.66ca3fed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nca-react/static/js/2.66ca3fed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25428990c6d730e232ac",
+    "url": "/nca-react/static/js/main.09cb3f6a.chunk.js"
+  },
+  {
+    "revision": "ff36c69584d18bdf4f6e",
+    "url": "/nca-react/static/js/runtime-main.8b7e1d09.js"
+  }
+]);
