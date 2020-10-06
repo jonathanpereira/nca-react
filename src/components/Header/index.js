e94d1864ae71dbcbc3aa7ef85caeca006731b496
JonathanPereira <jonathan@jonathanpereira.com>
2020-10-06 11:06:10 +0200

    feat: implement the html and css for the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,44 @@
-import React from "react";
+import React from 'react'
 
-const Header = () => <h2>Header</h2>;
+import Style from './style.module.scss'
 
-export default Header;
+const Header = () => {
+  return (
+    <nav className={Style.header}>
+      <a href="#" className={Style.logo}>
+        New Church Audio
+      </a>
+      <div>
+        <ul className={Style.menu}>
+          <li>
+            <a href="#" className={Style.active}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#">Events</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Speakers</a>
+          </li>
+          <li>
+            <a href="#">Donate</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">
+              <small>Desktop Version</small>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
